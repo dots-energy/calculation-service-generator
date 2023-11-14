83ddfabaf685c284c5c50180ca0e7a07e29a6c7f
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2023-11-14 15:16:48 +0100

    fix dockerhub push script

--- {{ cookiecutter.service_name }}/deploy/build_and_push_image_dockerhub.sh
+++ {{ cookiecutter.service_name }}/deploy/build_and_push_image_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# login first: docker login -u "dotsenergyframework
+# login first: docker login -u dotsenergyframework
 VERSION=0.0.1
 # CHANGE THE REPOSITORY:
-REPOSITORY=""dotsenergyframework/pv_installation_service"
+REPOSITORY="dotsenergyframework/pv_installation_service"
 
 docker build -t ${REPOSITORY}:${VERSION} ./..
 
